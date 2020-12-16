7be312ff49d8d5ffa962fd614faef69e23c3d3da
mahes <mahes@DESKTOP-RGBCTVB>
2020-12-15 21:05:45 -0600

    3rd commit

--- DevopsDemo/src/test/java/CICDDemo.java
+++ DevopsDemo/src/test/java/CICDDemo.java
@@ -3,7 +3,7 @@ public class CICDDemo {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("This is demo for buliding cicd pipeline and commit to branch");
+		System.out.println("This is demo for buliding cicd pipeline and 2nd commit to branch");
 	}
 
 }
